e565c1d0e4f08f2647a6bd07720f2d0bfa115049
John Koerner <john@JohnMacBookPro.local>
2017-06-30 19:16:50 -0400

    Initial checkin

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NetCoreSpecialFolders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (Environment.SpecialFolder sf in Enum.GetValues(typeof(System.Environment.SpecialFolder)))
+            {
+                Console.WriteLine($"{sf.ToString()}, {Environment.GetFolderPath(sf)}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
