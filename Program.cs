9dc68f0469ac3676d546d410506fed581ecf6bf7
John Koerner <github@riddlepop.com>
2017-07-03 11:35:39 -0400

    Updating for windows

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace NetCoreSpecialFolders
@@ -14,8 +15,19 @@ namespace NetCoreSpecialFolders
                 sb.AppendLine($"{sf.ToString()}, {Environment.GetFolderPath(sf)}");
             }
             var path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().FullName);
-            var filename = System.IO.Path.Combine(path, "macOut.csv");
-            System.IO.File.WriteAllText(filename, sb.ToString());
+            var fileName = GetFileName();
+            var filePath = System.IO.Path.Combine(path, $"{fileName}.csv");
+            System.IO.File.WriteAllText(filePath, sb.ToString());
+        }
+
+        static string GetFileName()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return "Win";
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                return "OSX";
+
+            return "Linux";
         }
     }
 }
